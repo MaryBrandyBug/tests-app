c5994e3dccfb442ab0af5762f4f2bf04f86207bb
Maria Bystrova <17mashmallow@gmail.com>
2024-04-07 03:58:54 +0400

    create universal Modal component

---
+++ src/components/commons/Modal/Modal.jsx
@@ -0,0 +1,44 @@
+'use client';
+
+import {
+  any, bool, func, string,
+} from 'prop-types';
+import cx from 'classnames';
+import Image from 'next/image';
+
+import Button from '../Button';
+
+import s from './Modal.module.scss';
+
+export default function Modal({
+  header, children, cancelation = false, onClick,
+}) {
+  return (
+    <div className={cx([s.root])}>
+      <div className={s.content}>
+        <div className={s.closeBtnContainer}>
+          <Button className={s.closeBtn} onClick={onClick}><Image src="/close.svg" alt="close button" width={50} height={50} /></Button>
+        </div>
+        <div className={s.headerContainer}>
+          <header className={s.header}>{header}</header>
+        </div>
+        <div className={s.contentContainer}>
+          {children}
+        </div>
+        {cancelation
+      && (
+      <div className={s.cancelationBtnContainer}>
+        <Button className={s.cancelationBtn} onClick={onClick}>Cancel</Button>
+      </div>
+      )}
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  header: string,
+  children: any,
+  cancelation: bool,
+  onClick: func,
+};
