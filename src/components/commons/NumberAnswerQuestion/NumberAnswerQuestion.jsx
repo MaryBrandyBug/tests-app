fbcea65cc877750f1d72de8fd2bf02bbde23999c
Maria Bystrova <17mashmallow@gmail.com>
2024-04-09 03:59:05 +0400

    add modal for confirmation to NumberAnswerQuestion

--- src/components/commons/NumberAnswerQuestion/NumberAnswerQuestion.jsx
+++ src/components/commons/NumberAnswerQuestion/NumberAnswerQuestion.jsx
@@ -1,16 +1,34 @@
 'use client';
 
+import { useState } from 'react';
 import { useFormik } from 'formik';
 
 import validationSchema from '@/utils/validation/NumberAnswerQuestionValidation';
 import data from './data';
 
+import Modal from '../Modal';
 import InputField from '../InputField';
 import Button from '../Button';
 
 import s from './NumberAnswerQuestion.module.scss';
 
 export default function NumberAnswerQuestion() {
+  const [openSaveConfirmation, setOpenSaveConfirmation] = useState(false);
+  const [openDeleteConfirmation, setOpenDeleteConfirmation] = useState(false);
+
+  const deleteConfirmation = () => {
+    setOpenDeleteConfirmation(true);
+  };
+
+  const saveConfirmation = () => {
+    setOpenSaveConfirmation(true);
+  };
+
+  const closeModal = () => {
+    if (openSaveConfirmation) setOpenSaveConfirmation(false);
+    if (openDeleteConfirmation) setOpenDeleteConfirmation(false);
+  };
+
   const onSubmit = async (values, actions) => {
     const isValid = validationSchema.isValid(values);
     if (isValid) {
@@ -22,8 +40,16 @@ export default function NumberAnswerQuestion() {
         body: JSON.stringify(values),
       });
     }
-
+    closeModal();
     actions.setSubmitting(false);
+  };
+
+  const onClick = async (values) => {
+    console.log(values);
+    const isValid = validationSchema.isValid(values);
+    if (isValid) {
+      setOpenSaveConfirmation(true);
+    }
   };
 
   const formik = useFormik({ initialValues: { title: '', answer: '', question_type: 'number' }, onSubmit, validationSchema });
@@ -39,21 +65,29 @@ export default function NumberAnswerQuestion() {
       placeholder={item.placeholder}
       maxLength={item.maxLength}
       additionalClass={s.formInput}
-      errorMessage={formik.touched[item.name] && formik.errors[item.name] && (
-      <div className={s.errorMessageContainer}><p className={s.errorMessage}>{formik.errors[item.name]}</p></div>
-      )}
+      errorMessage={formik.touched[item.name] && formik.errors[item.name] ? (
+        <div className={s.errorMessageContainer}><p className={s.errorMessage}>{formik.errors[item.name]}</p></div>
+      ) : null}
     />
   ));
 
   return (
     <div className={s.root}>
       <form className={s.form} onSubmit={formik.handleSubmit}>
+        { openSaveConfirmation && (
+        <Modal header="Do you want to save your question?">
+          <div>
+            <Button type="submit">Yes</Button>
+            <Button onClick={closeModal}>No</Button>
+          </div>
+        </Modal>
+        )}
         {inputFields}
-        <div className={s.btnContainer}>
-          <Button type="submit" className={s.saveBtn}>Save</Button>
-          <Button className={s.deleteBtn}>Delete</Button>
-        </div>
       </form>
+      <div className={s.btnContainer}>
+        <Button className={s.saveBtn} onClick={onClick}>Save</Button>
+        <Button className={s.deleteBtn} onClick={deleteConfirmation}>Delete</Button>
+      </div>
     </div>
   );
 }
