11572d9849f5c558c26f5b23639478663ea98c28
Maria Bystrova <17mashmallow@gmail.com>
2024-10-09 05:32:53 +0400

    add search error message to test library

--- src/components/commons/TestLibrary/TestLibrary.jsx
+++ src/components/commons/TestLibrary/TestLibrary.jsx
@@ -8,9 +8,9 @@ import cx from 'classnames';
 import TestCard from '../TestCard';
 import Pagination from '../Pagination';
 import Button from '../Button';
+import SearchField from '../SearchField';
 
 import s from './TestLibrary.module.scss';
-import SearchField from '../SearchField';
 
 export default function TestLibrary({ is_admin, onTestStarting }) {
   const [tests, setTests] = useState([]);
@@ -18,9 +18,19 @@ export default function TestLibrary({ is_admin, onTestStarting }) {
   const [totalPages, setTotalPages] = useState(1);
   const [sortTumblerSwitched, setSortTumblerSwitched] = useState(false);
   const [dateSorting, setDateSorting] = useState('created_at_desc');
+  const [searchQuery, setSearchQuery] = useState('');
 
   const router = useRouter();
   const { query } = router;
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+    router.push({
+      pathname: router.pathname,
+      query: { ...query, search: searchQuery },
+    }, undefined, { shallow: true });
+  };
+  const handleReset = () => setSearchQuery('');
 
   const switchSorting = () => {
     if (dateSorting === 'created_at_desc') {
@@ -48,7 +58,7 @@ export default function TestLibrary({ is_admin, onTestStarting }) {
 
   const fetchTests = async (page) => {
     try {
-      fetch(`https://interns-test-fe.snp.agency/api/v1/tests?page=${page}&sort=${dateSorting}`, {
+      fetch(`https://interns-test-fe.snp.agency/api/v1/tests?page=${page}&sort=${dateSorting}&search=${searchQuery}`, {
         method: 'GET',
         credentials: 'include',
         headers: {
@@ -92,10 +102,27 @@ export default function TestLibrary({ is_admin, onTestStarting }) {
   useEffect(() => {
     if (router.isReady) {
       fetchTests(currentPage);
+
+      if (!searchQuery) {
+        const { search, ...queryWithoutSearch } = query;
+        router.push({
+          pathname: router.pathname,
+          query: queryWithoutSearch,
+        }, undefined, { shallow: true });
+      }
     }
-  }, [currentPage, router.isReady, totalPages, dateSorting]);
+  }, [currentPage, router.isReady, totalPages, dateSorting, searchQuery]);
 
   const testLibrary = tests?.map((test) => <TestCard key={test.id} title={test.title} is_admin={is_admin} questionNumber={test.questions.length} id={test.id} onClick={handleStartTest} />);
+
+  const showCurrentLibrary = () => {
+    if (testLibrary.length > 0 && !searchQuery) return testLibrary; // есть созданные тесты && поисковое поле пустое
+    if (testLibrary.length > 0 && searchQuery) return testLibrary; // есть тесты удовлетворяющие поисковому запросу && поисковое поле не пустое
+    if (!testLibrary.length && searchQuery) return <p className={s.searchHasNoResult}>No tests matching your search were found.</p>; // нет тестов удовлетворяющих поисковому запросу && поисковое поле не пустует
+    return '';
+  };
+
+  const currentLibrary = showCurrentLibrary();
 
   const onSwitch = () => {
     setSortTumblerSwitched(!sortTumblerSwitched);
@@ -104,7 +131,7 @@ export default function TestLibrary({ is_admin, onTestStarting }) {
 
   return (
     <div className={s.root}>
-      <SearchField />
+      <SearchField handleChange={handleChange} value={searchQuery} onClick={handleReset} />
       <div className={s.dateSortContainer}>
         <p>NEWEST</p>
         <Button className={cx(s.tumblerContainer, { [s.tumblerRightPosition]: sortTumblerSwitched })} onClick={onSwitch}>
@@ -113,7 +140,7 @@ export default function TestLibrary({ is_admin, onTestStarting }) {
         <p>OLDEST</p>
       </div>
       <div className={s.content}>
-        {testLibrary}
+        {currentLibrary}
       </div>
       <Pagination currentPage={currentPage} totalPages={totalPages} onClick={handleFlipPage} />
     </div>
